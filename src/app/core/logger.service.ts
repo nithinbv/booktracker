39a0f4c57abef98e8e10dbfbb0ffc3180aeb4e36
nithinbv <nithin.bv@gmail.com>
2018-10-19 14:40:39 -0400

    Test Check
    Test Check

---
+++ src/app/core/logger.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LoggerService {
+  log(message: string): void {
+    const timeString: String = new Date().toLocaleTimeString();
+    console.log(`${message} (${timeString})`);
+  }
+
+  error(message: string): void {
+    const timeString: String = new Date().toLocaleTimeString();
+    console.error(`ERROR: ${message} (${timeString})`);
+  }
+}
